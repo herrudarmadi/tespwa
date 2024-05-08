fd65cda2c885ef1a16a24283cb0605bee5f987c5
herrudarmadi <137267873+herrudarmadi@users.noreply.github.com>
2024-05-08 13:29:46 +0700

    Update app.js

--- app.js
+++ app.js
@@ -8,3 +8,14 @@ async function registerSync() {
   const swRegistration = await navigator.serviceWorker.ready;
   swRegistration.sync.register("send-timer");
 }
+
+// Check if the browser is online or offline
+window.addEventListener('offline', () => {
+    // Show the offline message
+    document.getElementById('offlineMessage').style.display = 'block';
+});
+
+window.addEventListener('online', () => {
+    // Hide the offline message
+    document.getElementById('offlineMessage').style.display = 'none';
+});
