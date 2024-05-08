1c15895606197fe5928ac5ef99ecf3cdfc9c6833
herrudarmadi <137267873+herrudarmadi@users.noreply.github.com>
2024-05-08 16:38:25 +0700

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ function getTS() {
 let isSending, sendingInfoElement = document.getElementById("sendingInfo");
 
 async function registerSync() {
-  localStorage.setItem('testhd', 1234);
   isSending = true;
   sendingInfoElement.innerText = 'sending attempt..';
   const swRegistration = await navigator.serviceWorker.ready;
