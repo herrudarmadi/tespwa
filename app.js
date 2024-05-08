9c9dd3f8180ea4508fabe72877270be575f58a71
herrudarmadi <137267873+herrudarmadi@users.noreply.github.com>
2024-05-08 13:22:11 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-document.getElementById("timer").innerText = Date.now() / 1000 | 0; 
+document.getElementById("timer").innerText = getTS();
+
+function getTS() {
+  return Date.now() / 1000 | 0; 
+}
+
+async function registerSync() {
+  const swRegistration = await navigator.serviceWorker.ready;
+  swRegistration.sync.register("send-timer");
+}
